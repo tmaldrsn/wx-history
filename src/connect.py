8c14576134d95c5af7a728e067d132076cb2841e
Tim Alderson <tmaldrsn@gmail.com>
2019-01-11 05:01:33 -0500

    LOTS of refactoring done in db writer, fixed few bugs. currently stable.

--- src/connect.py
+++ src/connect.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import sqlite3
 import urllib.request
 from bs4 import BeautifulSoup
@@ -9,21 +10,25 @@ def is_db_path(db_path):
     return os.path.splitext(db_path)[-1].lower() == ".db"
 
 
-def get_db_connection(db_path):
-    """Returns a sqlite3 connection object"""
-    return sqlite3.connect(db_path)
-
-
 def get_db_cursor(db_path):
     """Returns a sqlite3 cursor object given a path to a database"""
     return sqlite3.connect(db_path).cursor()
 
 
-def get_observations_request(station_id):
+def get_observations_request(station_id, timeout=3):
     """Returns a urllib.request.Request object for the observation page of the station"""
-    return urllib.request.urlopen(f"https://w1.weather.gov/data/obhistory/{station_id}.html")
+    return urllib.request.urlopen(f"https://w1.weather.gov/data/obhistory/{station_id}.html", timeout=timeout)
 
 
-def get_observations_html(request):
-    """Returns the BeautifulSoup object"""
-    return BeautifulSoup(request, 'html.parser')
+def get_datetime_string(day, time=None):
+    """Returns the formatted string for the day and time input"""
+    if time:
+        return datetime.datetime.today().strftime(f"%m/{day}/%Y {time}")
+    return datetime.datetime.today().strftime(f"%m/{day}/%Y")
+
+
+def get_datetime(date_string, include_time=False):
+    """Returns the datetime object for the given string in appropriate format"""
+    if include_time:
+        return datetime.datetime.strptime(date_string, "%m/%d/%Y %H:%M")
+    return datetime.datetime.strptime(date_string, "%m/%d/%Y")
