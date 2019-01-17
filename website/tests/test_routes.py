59cce311c8e31a9329ed3d116ad9d9e31c8bad6b
Tim Alderson <tmaldrsn@gmail.com>
2019-01-17 15:54:57 -0500

    added more tests, ignored jinja & werkzeug test warnings

--- website/tests/test_routes.py
+++ website/tests/test_routes.py
@@ -3,13 +3,38 @@ import pytest
 from website import app
 
 
-@pytest.fixture()
+@pytest.fixture
 def client():
     app.config['TESTING'] = True
     client = app.test_client()
-    yield client
+    return client
+
+
+@pytest.fixture
+def valid_station():
+    return "KTOL"
+
+
+@pytest.fixture
+def invalid_station():
+    return "K000"
+
+
+@pytest.mark.parametrize("page,title", [
+    ('/', b"NOAA Hourly Weather Observation Database"),
+    ('/stations/', b"Stations List"),
+    ('/search/', b"Search Database")
+])
+def test_main_page_titles(client, page, title):
+    page = client.get(page)
+    assert title in page.get_data()
 
 
 def test_negative_page_numbers_return_404(client):
     page = client.get('/stations/KTOL/-1')
+    assert page.status_code == 404
+
+
+def test_invalid_station_return_404(client, invalid_station):
+    page = client.get('/ stations/' + invalid_station)
     assert page.status_code == 404
