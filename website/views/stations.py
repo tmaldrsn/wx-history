f8f2e0e3d8fca664436deb3486c480f933c247a2
Tim Alderson <tmaldrsn@gmail.com>
2019-03-07 17:13:41 -0500

    modified station pages to fit new db model & updated pipfile

--- website/views/stations.py
+++ website/views/stations.py
@@ -116,38 +116,43 @@ mod = Blueprint('stations', __name__, url_prefix='/stations/')
 
 @mod.route('/')
 def show_station_list():
-    con = sqlite3.connect("observations.db")
+    station_df = pd.read_csv('stations.csv', sep=',',
+                             quotechar="|", nrows=2190)
+    """
+    con = sqlite3.connect("observations-new.db")
     cur = con.cursor()
 
     query = "select * from station"
     stations_list = list(cur.execute(query))
     con.close()
-    return render_template('stations/index.html', stations=stations_list)
+    """
+    return render_template('stations/index.html', stations=station_df.values)
 
 
 @mod.route('/<s>/')
 def show_station_information(s):
-    con = sqlite3.connect("observations.db")
+    station_df = pd.read_csv('stations.csv', sep=',',
+                             quotechar="|", nrows=2190)
+    station_info = station_df[station_df['ID'] == s].values
+
+    con = sqlite3.connect('observations-new.db')
     cur = con.cursor()
-
-    query = f"select * from station where id='{s}'"
-    station_info = list(cur.execute(query))
-
     query = f"select * from {s} limit 1"
     current_info = list(cur.execute(query))
-
     con.close()
+
     return render_template('stations/dashboard.html', station_info=station_info, current_info=current_info)
 
 
 @mod.route('/<s>/<page>')
 def show_station_data(s, page=1):
-    con = sqlite3.connect("observations.db")
+    con = sqlite3.connect("observations-new.db")
     cur = con.cursor()
 
-    station_query = f"select * from station where id='{s}'"
-    station_data = list(cur.execute(station_query))
-    query = f"select * from {s} order by substr(date, 7, 4) desc, substr(date, 1, 2) desc, substr(date, 4, 2) desc, time desc limit 50 offset {50*(int(page)-1)}"
+    station_df = pd.read_csv('stations.csv', sep=',',
+                             quotechar="|", nrows=2190)
+    station_info = station_df[station_df['ID'] == s].values
+    query = f"select * from {s} order by datetime desc limit 50 offset {50*(int(page)-1)}"
     observations = list(cur.execute(query))
     len_query = f"select count(*) from {s}"
     num_observations = list(cur.execute(len_query))[0][0]
@@ -156,7 +161,7 @@ def show_station_data(s, page=1):
 
     if int(page) > max_page or int(page) < 0:
         abort(404)
-    return render_template('stations/observations.html', station=station_data, obs=observations, page=int(page),  max_page=int(max_page))
+    return render_template('stations/observations.html', station=station_info, obs=observations, page=int(page),  max_page=int(max_page))
 
 
 @mod.route('/<s>/current')
