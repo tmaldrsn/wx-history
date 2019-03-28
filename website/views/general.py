6f6a9255c33b3cba69490db675eca5eaba3d51df
Tim Alderson <tmaldrsn@gmail.com>
2019-03-28 00:10:54 -0400

    added real-time clock and daily extremes div to front page

--- website/views/general.py
+++ website/views/general.py
@@ -8,6 +8,7 @@ from flask import (
 )
 import sqlite3
 import datetime
+import pandas as pd
 
 mod = Blueprint('general', __name__)
 
@@ -15,6 +16,36 @@ mod = Blueprint('general', __name__)
 @mod.route('/')
 def home_page():
     return render_template('general/index.html')
+
+
+@mod.route('/_get_extremes')
+def extremes():
+    con = sqlite3.connect('data/observations.db')
+    cur = con.cursor()
+
+    station_df = pd.read_csv('data/stations.csv', sep=',', quotechar="|")
+    station_list = station_df.ID.values
+
+    min_temp, max_temp = 100, 0
+    min_station, max_station = "", ""
+    dt = datetime.date.today()
+    for station in station_list:
+        query = f"""select * from {station} where substr(datetime, 0, 11)='{str(dt)}' order by "%s" %s limit 1"""
+        try:
+            min_query = query % ("Air Temperature", "desc")
+            min_station_temp = list(cur.execute(min_query))[0][5]
+            max_query = query % ("Air Temperature", "asc")
+            max_station_temp = list(cur.execute(max_query))[0][5]
+
+            if min_station_temp < min_temp:
+                min_temp = min_station_temp
+                min_station = station
+            if max_station_temp > max_temp:
+                max_temp = max_station_temp
+                max_station = station
+        except (IndexError, TypeError):
+            pass
+    return jsonify(date=str(dt), low=min_temp, low_station=min_station, high=max_temp, high_station=max_station)
 
 
 @mod.route('/_add_numbers')
